efcf04b269b99fcd22545629b26be53725e27e61
Roland Dordevic <rdorde@gmail.com>
2022-04-18 17:31:44 +0200

    get temp from api

--- App.js
+++ App.js
@@ -1,12 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import * as Location from 'expo-location';
+import { useEffect, useState } from 'react';
+
+const WEATHER_API_BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 export default function App() {
+  const [weather, setWeather] = useState(null);
+  const [errorMsg, setErrorMsg] = useState('');
+
+  async function askLocation() {
+    try {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      const location = await Location.getCurrentPositionAsync();
+      const { latitude, longitude } = location.coords;
+
+      const weatherApiUrl = `${WEATHER_API_BASE_URL}?lat=${latitude}&lon=${longitude}&units=metric&appid=${process.env.WEATHER_API_KEY}`;
+      const response = await fetch(weatherApiUrl);
+      const result = await response.json();
+
+      if (response.ok) {
+        setWeather(result);
+        console.log('result', result);
+      } else {
+        setErrorMsg(result?.message);
+      }
+    } catch (error) {
+      setErrorMsg('Something went wrong.');
+    }
+  }
+  useEffect(() => askLocation(), []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={() => console.log('text clicked')}>
+        {weather ? weather?.main?.temp : errorMsg}
+      </Text>
+      <StatusBar style='auto' />
+    </SafeAreaView>
   );
 }
 
@@ -15,6 +51,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
